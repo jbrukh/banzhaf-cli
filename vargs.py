77e17519b44f61c30f6106f011fd15574ef9a9d5
Jake Brukhman <jake@coinfund.io>
2019-11-26 20:52:27 -0500

    vargs

--- vargs.py
+++ vargs.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
+
+def process(x, divisor=1):
+    x = x.strip('"\n')
+    x = float(x) / divisor
+    x = round(x, 0)
+    return int(x)
 
 if __name__ == '__main__':
 
-    weights = sorted([int(float(line)) for line in sys.stdin], reverse=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', dest='divisor', type=int, default=1)
+    args = parser.parse_args()
+
+    weights = sorted([process(line, args.divisor) for line in sys.stdin], reverse=True)
     weights = [weight for weight in weights if weight != 0]
     quota = int(sum(weights) / 2) + 1
 
